2ca894df7769ce963209c19dda489608b16f138e
Michael Sevilla <mikesevilla3@gmail.com>
2016-04-18 09:26:16 -0700

    roles: update SRL roles to include monitoring
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/emaster.sh
+++ experiments/emaster.sh
@@ -6,10 +6,18 @@ set -x
 DIR=`pwd`
 ROOTDIR=`dirname $DIR`
 
+# adding a screen
+SCREEN=`echo $DISPLAY | sed s/localhost//g | sed 's/\.0//g'`
+XAUTH=`xauth list | grep $SCREEN`
+
 docker run --rm -it \
   --name="emaster" \
   --net=host \
   -v $ROOTDIR:/experiments \
+  -v /tmp:/tmp \
   -v ~/.ssh:/root/.ssh \
   --workdir=/experiments \
+  -e XAUTH="$XAUTH" \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
   michaelsevilla/emaster
