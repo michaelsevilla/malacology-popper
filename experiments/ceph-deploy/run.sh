9795eb14fa8bce83a71d0f428000439eaa2d1ff1
Michael Sevilla <mikesevilla3@gmail.com>
2016-05-08 02:22:17 -0700

    client: add ceph-fuse mount/shared stuff
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/ceph-deploy/run.sh
+++ experiments/ceph-deploy/run.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
-for w in test; do
-  for s in ceph-ext4; do
-    ANSIBLE_LOG_PATH="logs/$w-$s.log" \
-    ansible-playbook --skip-tags "with_pkg" $s.yml workloads/$w.yml
+for i in 0 1 2 3 4 5; do
+  for w in test; do
+    for s in ceph-ext4; do
+      ANSIBLE_LOG_PATH="logs/$w-$s-$i.log" \
+      ansible-playbook --skip-tags "with_pkg" $s.yml workloads/$w.yml
+    done
   done
 done
