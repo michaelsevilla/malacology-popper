f6155a51e767ee3d69747f9057e585e42d3672fc
Noah Watkins <noahwatkins@gmail.com>
2016-10-19 14:33:26 -0700

    updates
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- experiments/zlog-seqr-redux/run.sh
+++ experiments/zlog-seqr-redux/run.sh
@@ -6,14 +6,17 @@ set -x
 CEPH_BIN=/home/nwatkins/src/ceph/build/bin
 export CEPH_CONF=/home/nwatkins/src/ceph/build/ceph.conf
 
-prefix=$1
 runtime=120
 quota=0
 nclients=8
+#prefix=$1
 #capdelay=1.0
 
-for capdelay in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125; do
-  perf_file="${prefix}.nc-${nclients}.cd-${capdelay}.qa-${quota}"
-  $CEPH_BIN/seq-client --runtime=$runtime --perf_file=${perf_file} \
-    --instances=$nclients --capdelay=$capdelay --quota=${quota}
+for nclients in 2 4 8; do
+  prefix="${nclients}c_shrink_capdelay"
+  for capdelay in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125; do
+    perf_file="${prefix}.nc-${nclients}.cd-${capdelay}.qa-${quota}"
+    $CEPH_BIN/seq-client --runtime=$runtime --perf_file=${perf_file} \
+      --instances=$nclients --capdelay=$capdelay --quota=${quota}
+  done
 done
