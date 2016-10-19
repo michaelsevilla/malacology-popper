3e2e350fa511c6732483213798632d5f1aa1cc1d
Noah Watkins <noahwatkins@gmail.com>
2016-10-19 11:59:06 -0700

    updates
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- experiments/zlog-seqr-redux/run.sh
+++ experiments/zlog-seqr-redux/run.sh
@@ -7,11 +7,12 @@ CEPH_BIN=/home/nwatkins/src/ceph/build/bin
 export CEPH_CONF=/home/nwatkins/src/ceph/build/ceph.conf
 
 prefix=$1
-runtime=60
-capdelay="1.0"
+runtime=120
 quota=0
-nclients=4
-for capdelay in 1.0 0.5 0.25 0.125 .0625 0.03125; do
+nclients=2
+#capdelay=1.0
+
+for capdelay in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125; do
   perf_file="${prefix}.nc-${nclients}.cd-${capdelay}.qa-${quota}"
   $CEPH_BIN/seq-client --runtime=$runtime --perf_file=${perf_file} \
     --instances=$nclients --capdelay=$capdelay --quota=${quota}
