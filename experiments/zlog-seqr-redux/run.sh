1e70752dc4388ea0531340ad5190ada4996feb76
Noah Watkins <noahwatkins@gmail.com>
2016-10-19 12:36:50 -0700

    update
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- experiments/zlog-seqr-redux/run.sh
+++ experiments/zlog-seqr-redux/run.sh
@@ -9,7 +9,7 @@ export CEPH_CONF=/home/nwatkins/src/ceph/build/ceph.conf
 prefix=$1
 runtime=120
 quota=0
-nclients=2
+nclients=4
 #capdelay=1.0
 
 for capdelay in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125; do
